b6bd2f70fce85518521a02ffba61d780c9ef8561
Muhammad Zohaib Warraich <151923903+zohaibwarraich1@users.noreply.github.com>
2024-08-28 19:32:53 +0500

    Intro to DSA Folder in form of code file

---
+++ DSA_Folder_Intro.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "In this DSA Repositery all the data structures topics are covered." << endl;
+    cout << "1. Stacks" << endl;
+    cout << "2. Queues" << endl;
+    cout << "3. Lists" << endl;
+    cout << "4. Prefix/Postfix/Infix Conversions using stacks" << endl;
+    cout << "5. Trees" << endl;
+    cout << "   5.1 Binary Tree ( We didn't made a separate cpp file of Binary trees because if you understand the concept of Binary Search Tree than Binary Trees are not difficult for you :)" << endl;
+    cout << "   5.2 Binary Search Tree" << endl;
+    cout << "   5.3 AVL Tree" << endl;
+    cout << "   5.4 Heap Tree" << endl;
+    cout << "6. Graphs" << endl
+         << endl;
+    cout << "We didnt code for hashing but if you just undersatnd the concept and working of hashing from any resources like what is hashing and why is it used.\nWhat are the types of collision and how to deal with them etc.\nAfter that we can use built in hash containers like Maps or Sets for easability :)\nHappy Coding :)" << endl;
+    return 0;
+}
